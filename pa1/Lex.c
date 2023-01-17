0cbfb412892b478f7863927fbe9439931dcfd098
Nadia Shabbar <nshabbar@ucsc.edu>
2023-01-17 14:55:37 -0800

    Adding makefile, listclient, and listtest.

---
+++ pa1/Lex.c
@@ -0,0 +1,11 @@
+//-----------------------------------------------------------------------------
+// Lex.c
+// Implementation file for Lex sorting.
+//-----------------------------------------------------------------------------
+#include<stdio.h>
+#include<stdlib.h>
+#include<assert.h>
+#include<stdbool.h>
+#include<string.h>
+#include "List.h"
+
---
+++ pa1/ListTest.c
@@ -0,0 +1,63 @@
+//-----------------------------------------------------------------------------
+// ListTest.c
+// A test client for List ADT
+//-----------------------------------------------------------------------------
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+#include"List.h"
+
+int main(int argc, char* argv[]){
+	int i;
+	List A = newList();
+	// List B = newList();
+	
+	
+	// Prepend Test
+	//-----------------------------------------------------------------------------
+	printf("Testing prepend() function of List.c\n");
+	
+	for (i = 1; i<= 10; i++){
+		prepend(A, i);
+		// prepend(B, i);
+	}
+	
+	printf("Printing List A: ");
+	printList(A);
+	printf("\n");
+	// printf("Printing List B: ");
+	// printList(B);
+	// printf("\n");
+	
+	
+	// Append Test
+	//-----------------------------------------------------------------------------
+	printf("\n");
+	printf("Testing append() function of List.c\n");
+	
+	for (i = 10; i <= 15; i++){
+		append(A, i);
+		// append(B, i);
+	}
+	
+	printf("Printing List A: ");
+	printList(A);
+	printf("\n");
+	// printf("Printing List B: ");
+	// printList(B);
+	// printf("\n");
+	
+	
+	// Copy List Test
+	//-----------------------------------------------------------------------------
+	printf("\n");
+	printf("Testing copyList() function of List.c\n");
+	list B = copyList(A);
+	return(B);
+	
+	
+	// Equals Test
+	//-----------------------------------------------------------------------------
+	
+	
+	
