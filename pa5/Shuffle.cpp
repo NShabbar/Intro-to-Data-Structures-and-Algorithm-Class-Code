75b138ed0d27caf6215d70677d77445b22474557
Nadia Shabbar <nshabbar@ucsc.edu>
2023-02-22 12:21:40 -0800

    Finished pa5.

--- pa5/Shuffle.cpp
+++ pa5/Shuffle.cpp
@@ -10,35 +10,55 @@
 #include<stdexcept>
 #include"List.h"
 
+using namespace std;
+
 void shuffle(List& D){
 	List A;
 	List B;
-	for (D.moveFront(); D.position() < (D.length()/2); D.moveNext()){
+	D.moveFront();
+	while (D.position() < (D.length()/2)){
 		A.insertBefore(D.moveNext());
 	}
-	for (D.position(); D.position() < D.length(); D.moveNext()){
+	while (D.position() < D.length()){
 		B.insertBefore(D.moveNext());
 	}
 	D.clear();
 	A.moveFront();
 	B.moveFront();
-	while (A.position() >= 0 && B.position() >= 0){
-		D.insertAfter(B.moveNext());
-		D.insertAfter(A.moveNext());
+	while (A.position() != A.length() && B.position() != B.length()){
+		D.insertBefore(B.moveNext());
+		D.insertBefore(A.moveNext());
 	}
-	while (A.position() >= 0){
+	/* while (A.position() != A.length()){
 		D.insertAfter(A.moveNext());
-	}
-	while (B.position() >= 0){
-		D.insertAfter(A.moveNext());
+	} */
+	while (B.position() != B.length()){
+		D.insertBefore(B.moveNext());
 	}
 }
 
-int main(int n){
-	List D;
-	for (int i = 0; i < n; i++){
-		D.insertBefore(i);
+int main(int argc, char* argv[]){
+	if (argc !=2){
+		cerr << "Usage: " << argv[0] << "incorrect value." << endl;
+		return(EXIT_FAILURE);
 	}
+	int n = atoi(argv[1]);
+	List D;
+	//cout << "Shuffle " << n << endl;
+	cout << "deck size \t shuffle count" << endl;
+	cout << "------------------------------" << endl;
 	List C = List(D);
-	C.shuffle(D);	
+	
+	for (int deck_count = 1; deck_count < n+1; deck_count++){
+		int shuffle_count = 0;
+		D.insertBefore(deck_count);	
+		C = List(D);
+		shuffle(D);
+		shuffle_count++;
+		while (!(C == D)){
+			shuffle(D);
+			shuffle_count++;
+		}
+		cout << " " << deck_count << "\t \t  " << shuffle_count << endl;
+	}
 }
