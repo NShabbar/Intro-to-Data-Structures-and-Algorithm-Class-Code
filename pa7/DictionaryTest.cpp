38a3e3941e7332effaa7641919cdedd5a029bb04
Nadia Shabbar <nshabbar@ucsc.edu>
2023-03-10 11:01:48 -0800

    Finished assignment.

--- pa7/DictionaryTest.cpp
+++ pa7/DictionaryTest.cpp
@@ -13,18 +13,48 @@
 using namespace std;
 int main(){
 	Dictionary A;
-	std::string ideal = "a\nb\ne\nh\nf\ni\n";
-    A.setValue("a", 1);
-    A.setValue("b", 5);
-    A.setValue("e", 10);
-    A.setValue("h", 15);
-    A.setValue("f", 20);
-    A.setValue("i", 100);
+	
+    A.setValue("A", 6);
+    A.setValue("B", 9);
+    A.setValue("G", 11);
+    A.setValue("V", 15);
+    A.setValue("X", 13);
+    A.setValue("Z", 100);
     std::string val = A.pre_string();
-    if (val != ideal){
-		cout << "val: " <<val<<endl;
-		cout << "ideal: " <<ideal<<endl;
-      return 1;
-	}
-    return 0;
+	
+	cout<<val<<endl;
+	
+	cout<<"Testing pre_string(): "<<val<<endl;
+	
+	A.begin();
+	cout<<"Testing begin(): "<<A.currentKey()<<endl;
+	
+	A.next();
+	cout<<"Testing next(): "<<A.currentKey()<<endl;
+	
+	A.end();
+	cout<<"Testing end(): "<<A.currentKey()<<endl;
+	
+	A.prev();
+	cout<<"Testing prev(): "<<A.currentKey()<<endl;
+	
+	cout<<"Testing contains('B'): "<<A.contains("B")<<endl;
+	
+	cout<<"Testing size(): "<<A.size()<<endl;
+	
+	cout<<"Testing getValue('B'): "<< A.getValue("B")<<endl;
+	
+	cout<<"Testing hasCurrent(): "<<A.hasCurrent()<<endl;
+	
+	cout<<"Testing currentKey(): "<<A.currentKey()<<endl;
+	
+	cout<<"Testing currentVal(): "<<A.currentVal()<<endl;
+	
+	Dictionary B = A;
+	
+	cout<<"Testing equals(): "<<A.equals(B)<<endl;
+	
+	A.clear();
+	cout<<"Testing clear(): "<<A<<endl;
+
 }
